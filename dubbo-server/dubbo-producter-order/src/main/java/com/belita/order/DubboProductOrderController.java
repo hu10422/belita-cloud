05cdfa4568e53237692dfd65d9be3307153d5a36
hu10422 <1662143230@qq.com>
2021-05-10 22:22:45 +0800

    dubbo 相关

---
+++ dubbo-server/dubbo-producter-order/src/main/java/com/belita/order/DubboProductOrderController.java
@@ -0,0 +1,12 @@
+package com.belita.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboProductOrderController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProductOrderController.class, args);
+    }
+}
